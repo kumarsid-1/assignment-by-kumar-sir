66533477e80c997c94768720d9ce0fc8c4ea2a7e
kumarsid-1 <88184718+kumarsid-1@users.noreply.github.com>
2022-11-19 18:46:25 +0530

    Update DemoController.php

--- MY ASSIGNMENT/DemoController.php
+++ MY ASSIGNMENT/DemoController.php
@@ -10,7 +10,7 @@ class DemoController extends Controller
     public function index()
     {
         $evar= DB::table('employees')
-        
+        ->select('employee_id', 'name', 'DATEDIFF(SYSDATE(), DOJ')/365 as years)
         ->get();
 
         return view('employee.index', ['employees' => $evar]);
